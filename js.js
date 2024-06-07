d01e6ecdd3618ca6b5c4225b83367857874a2876
salmanpoomonmlp <salman567633800@gmail.com>
2024-06-07 14:47:25 +0530

    pattern in javascript

--- js.js
+++ js.js
@@ -1,4 +1,20 @@
-let cars = ['bmw', 'supra',true,1,null]
+var patern =""
+var n=5
 
-cars[9] = 7;
-console.log(cars)
+for(var i=1;i<=n;i++){
+  for(var k=1;k<=i;k++){
+    patern +=" "
+   }
+   for(var j=1;j<=(n-i)*2+1;j++){
+    if(i==1 || j==1|| j== (n-i)*2+1){
+    patern+="*"
+    }else{
+      patern+=" "
+    }
+     }
+  
+
+
+    patern +="\n"
+}
+console.log(patern)
