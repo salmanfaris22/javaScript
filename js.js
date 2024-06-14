56e23b5277d661eb5897c6715629b299592d88f6
salmanpoomonmlp <salman567633800@gmail.com>
2024-06-14 09:56:03 +0530

    metion test

--- js.js
+++ js.js
@@ -1,7 +1,16 @@
-const person = {
-    ins : "instagram",
-    cro : "saalal"
+const arr = ["salman","faris","from","indai"]
+
+
+arr.forEach(upperCase)
+x =arr.forEach(display)
+
+function upperCase(element ,index, arr){
+    return arr[index] = element.charAt(0).toUpperCase()  + element.slice(1)
 }
-for(let string in person){
-console.log(string ,person[string])
+
+function display(element){
+  return console.log(element)
 }
+
+
+console.log()
