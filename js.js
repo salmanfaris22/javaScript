c0f367869cd7ccca39701bbb5650702917e2a1f8
salmanpoomonmlp <salman567633800@gmail.com>
2024-06-15 15:39:11 +0530

    metion test

--- js.js
+++ js.js
@@ -1,16 +1,5 @@
-const arr = ["salman","faris","from","indai"]
+a= [1,2,3,4,5]
 
-
-arr.forEach(upperCase)
-x =arr.forEach(display)
-
-function upperCase(element ,index, arr){
-    return arr[index] = element.charAt(0).toUpperCase()  + element.slice(1)
+for(x of a){
+    console.log(x)
 }
-
-function display(element){
-  return console.log(element)
-}
-
-
-console.log()
